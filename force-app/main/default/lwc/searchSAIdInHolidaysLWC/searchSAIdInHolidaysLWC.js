a22283459c458a3e37096b52c13c5f9fdbd3020c
victorovgoncalves <victor.fegunesp@gmail.com>
2021-08-03 11:11:51 -0300

    Commit 3

--- force-app/main/default/lwc/searchSAIdInHolidaysLWC/searchSAIdInHolidaysLWC.js
+++ force-app/main/default/lwc/searchSAIdInHolidaysLWC/searchSAIdInHolidaysLWC.js
@@ -1,5 +1,8 @@
 import { LightningElement,api,track,wire} from 'lwc';
 import searchSouthAfricaIdNumber from '@salesforce/apex/searchSAIdLWController.searchSouthAfricaIdNumber';
+import getRecordHolidaysByCallout from '@salesforce/apex/searchSAIdLWController.getRecordHolidaysByCallout';
+import insertHolidayRecords from '@salesforce/apex/searchSAIdLWController.insertHolidayRecords';
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
 
 export default class SearchSAIdInHolidaysLWC extends LightningElement {
     inputSouthAfricaIdNumber = '';
@@ -17,6 +20,7 @@ export default class SearchSAIdInHolidaysLWC extends LightningElement {
     showContactFromDatabase = false;
     
     changedSouthAfricaIdNumber(event){
+        this.showContactFromDatabase = false;
         this.inputSouthAfricaIdNumber = event.target.value;
         var newvalue = this.inputSouthAfricaIdNumber;
         let inputfield = this.template.querySelector(".inputSAField");
@@ -98,7 +102,7 @@ export default class SearchSAIdInHolidaysLWC extends LightningElement {
     }
     searchId(event)
     {
-        this.hideContactFromDatabase = true;
+        this.showContactFromDatabase = false;
         console.log('inputSouthAfricaIdNumber : '+this.inputSouthAfricaIdNumber);
         console.log('inputBirthdate : '+this.inputBirthdate);
         console.log('inputGender : '+this.inputGender);
@@ -116,15 +120,126 @@ export default class SearchSAIdInHolidaysLWC extends LightningElement {
         searchSouthAfricaIdNumber({contactSouthAfricaIdNumber: this.inputSouthAfricaIdNumber, contactBirthdayDate: this.inputBirthdate, contactGender: this.inputGender,contactCitizen: this.inputCitizenship})
             .then(result => {
                 console.log('retorno do apex:'+JSON.stringify(result));
-                var contactWasInserted = true;
-                if (contactWasInserted) {                    
-                    //this.fireSuccessToast();
+                console.log('key[0]:'+Object.keys(result)[0]);
+                
+                
+                var contactWasUpserted = true;
+                if(Object.keys(result)[0] != '')
+                {
+                    contactWasUpserted = false;
+                }
+
+                if (contactWasUpserted) {         
+                    console.log('contact upserted');
+                    console.log('values[0]:'+Object.values(result)[0]);
+                    var upsertedContactRecord = Object.values(result)[0];
+                
+                    this.outputSouthAfricaIdNumber = Object.values(result)[0].South_Africa_Id_Number__c;
+                    this.outputBirthdate = Object.values(result)[0].Birthdate;
+                    this.outputCitizenship = Object.values(result)[0].Citizenship__c;
+                    this.outputTimesSearched = Object.values(result)[0].Times_Searched__c;
+                    
+                    getRecordHolidaysByCallout({contactRecord: Object.values(result)[0]})
+                    .then(result => {
+
+                        var returnedCalloutMap = result;
+                        console.log('retorno do apex callout method: '+JSON.stringify(returnedCalloutMap));                      
+
+                        if(Object.keys(returnedCalloutMap)[0] != 'We cannot find any holidays on your birthday, based on your South Africa Id number')
+                        {
+                            var holidaysOnBithdayJson = Object.values(returnedCalloutMap)[0];                    
+                            var holidaysOnBithdayObj = JSON.parse(holidaysOnBithdayJson);
+                            var holidaysOnBithdaySize = holidaysOnBithdayObj.response.holidays.length;
+                            var holidaysResume = '';
+                            var holidays = '';
+                            var i = 0;
+                            console.log('holidaysOnBithdaySize:'+holidaysOnBithdaySize);
+                            console.log('holidaysOnBithdayJson:'+holidaysOnBithdayJson);
+                            for(i=0; i<holidaysOnBithdaySize ; i++)
+                            {                        
+                                if(holidaysOnBithdayObj.response.holidays[i].date.iso == this.inputBirthdate.replace(/ /g, '-'))
+                                {                            
+                                    holidaysResume = holidaysResume+holidaysOnBithdayObj.response.holidays[i].description;
+                                    if(holidays != '')
+                                    {
+                                        holidays = holidays + ',';
+                                    }
+                                    holidays = holidays + '{"Name":"'+holidaysOnBithdayObj.response.holidays[i].name+'",';
+                                    holidays = holidays + '"Description__c":"'+holidaysOnBithdayObj.response.holidays[i].description+'",';
+                                    holidays = holidays + '"Date__c":"'+holidaysOnBithdayObj.response.holidays[i].date.iso+'",';
+                                    holidays = holidays + '"Type__c":"'+holidaysOnBithdayObj.response.holidays[i].type+'"}';
+                                }
+                            }
+
+                            console.log('holidays to Apex: '+holidays);
+
+                            if(holidays != '')
+                            {                                
+                                insertHolidayRecords({holidayList: holidays,contactRecord: upsertedContactRecord})
+                                .then(result => {
+                                    console.log('result of inserted holiday Records'+result);
+                                })
+                                .catch(error => {
+                                    this.dispatchEvent(
+                                        new ShowToastEvent({
+                                            title: 'Error',
+                                            type: 'error',
+                                            message: error.toString(),                           
+                                        }),
+                                    );                
+                                });
+                            }
+
+                            console.log('holidays:'+holidays);
+
+                            this.dispatchEvent(
+                                new ShowToastEvent({
+                                    title: Object.keys(returnedCalloutMap)[0],
+                                    variant: 'success',
+                                    message: holidaysResume,             
+                                }),
+                            );
+                        }
+                        else{
+                            this.dispatchEvent(
+                                new ShowToastEvent({
+                                    title: 'Sorry',
+                                    type: 'error',
+                                    message: Object.keys(returnedCalloutMap)[0],                           
+                                }),
+                            );
+                        }
+                        this.showContactFromDatabase = true;
+                    })
+                    .catch(error => {
+                        this.dispatchEvent(
+                            new ShowToastEvent({
+                                title: 'Error',
+                                type: 'error',
+                                message: error,                           
+                            }),
+                        );            
+                    });
+
+                    
                 } else {
-                    console.log('First name was NOT updated');
+                    this.dispatchEvent(
+                        new ShowToastEvent({
+                            title: 'Error',
+                            type: 'error',
+                            message: Object.keys(result)[0],                           
+                        }),
+                    );
                 }
             })
             .catch(error => {
-                console.log('error: ', error);
+                this.dispatchEvent(
+                    new ShowToastEvent({
+                        title: 'Error',
+                        type: 'error',
+                        message: error,                           
+                    }),
+                );                
             });
     
 
